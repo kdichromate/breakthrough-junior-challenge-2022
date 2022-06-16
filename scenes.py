8594162971b20dd092bd058e04d1670c3a2c3878
kdichromate <itsyatharthg@gmail.com>
2022-06-16 20:29:03 +0530

    ✨ Problem statement scene

--- scenes.py
+++ scenes.py
@@ -1,14 +1,20 @@
 from manim import *
 from math import sin, cos
 
+
 class IntroScene(Scene):
+    """
+    Scene for the intro of the video.
+    """
+
     def construct(self):
         MAX_SIDES = 8
         ref_shape = None
         for side_n in range(3, MAX_SIDES + 1):
             # Regular Polygon animation
             angle = 2 * PI / side_n
-            co_ords = [[sin(i * angle), cos(i * angle), 0] for i in range(side_n)]
+            co_ords = [[sin(i * angle), cos(i * angle), 0]
+                       for i in range(side_n)]
             polygon = Polygon(*co_ords)
             polygon.set_fill(random_bright_color(), opacity=0.5)
 
@@ -16,7 +22,20 @@ class IntroScene(Scene):
                 ref_shape = polygon
                 self.add(ref_shape)
             else:
-                self.play(Transform(ref_shape, polygon))
-        
-        self.play(Transform(ref_shape, Text("Hello, World!")))
+                self.play(ReplacementTransform(ref_shape, polygon))
+                ref_shape = polygon
+
+        self.play(ReplacementTransform(ref_shape, Tex("Hello, World!")))
         self.wait(2)
+
+
+class ProblemStatingScene(Scene):
+    """
+    Scene for stating the problem statement.
+    """
+
+    def construct(self):
+        statement = Tex(
+            r"What is the the sum of all the natural numbers?\\1+2+3+4+...+n=?")
+        self.play(Write(statement))
+        self.wait(3)
