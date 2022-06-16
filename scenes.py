906b345422404e21c92bebb5dab6147cf4004783
kdichromate <itsyatharthg@gmail.com>
2022-06-16 13:33:56 +0530

    ✨ Created the intro scene

--- scenes.py
+++ scenes.py
@@ -1,8 +1,22 @@
 from manim import *
+from math import sin, cos
 
-
-class HelloScene(Scene):
+class IntroScene(Scene):
     def construct(self):
-        circle = Circle()
-        self.play(Create(circle))
-        self.play(circle.animate.set_fill(RED, opacity=0.5))
+        MAX_SIDES = 8
+        ref_shape = None
+        for side_n in range(3, MAX_SIDES + 1):
+            # Regular Polygon animation
+            angle = 2 * PI / side_n
+            co_ords = [[sin(i * angle), cos(i * angle), 0] for i in range(side_n)]
+            polygon = Polygon(*co_ords)
+            polygon.set_fill(random_bright_color(), opacity=0.5)
+
+            if not ref_shape:
+                ref_shape = polygon
+                self.add(ref_shape)
+            else:
+                self.play(Transform(ref_shape, polygon))
+        
+        self.play(Transform(ref_shape, Text("Hello, World!")))
+        self.wait(2)
